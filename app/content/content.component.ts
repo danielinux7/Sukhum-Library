f48520f149135ea0800ff01fbbf32237461d51e1
Daniel Abzakh <daniel.abzakh@gmail.com>
2016-07-15 01:14:37 +0400

    1- Added home page to content. 2- Added Apsni font support. 3- Added subject to contacts page.

--- app/content/content.component.ts
+++ app/content/content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
 import { ContactsComponent } from './contacts/contacts.component';
 @Component({
   selector: 'lib-content',
   templateUrl: './app/content/content.component.html',
   styleUrls:  ['./app/content/content.component.css'],
-  directives:[NewsComponent, ContactsComponent]
+  directives:[HomeComponent, NewsComponent, ContactsComponent]
 })
 export class ContentComponent { }
