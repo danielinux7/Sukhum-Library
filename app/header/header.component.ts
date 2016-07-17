210ae69740fb1ca49dcfe0c14fc96da8e0cd1a5c
Daniel Abzakh <daniel.abzakh@gmail.com>
2016-07-17 23:09:45 +0400

    Route home from gerb image in the header.

--- app/header/header.component.ts
+++ app/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 @Component({
   selector: 'lib-header',
   templateUrl: './app/header/header.component.html',
-  styleUrls:  ['./app/header/header.component.css']
+  styleUrls:  ['./app/header/header.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class HeaderComponent {
   title = 'Национальная Библиотека Абхазия';
