bf37b7fde63da73989477962272ef8d5a6b91e6e
Daniel Abzakh <daniel.abzakh@gmail.com>
2016-07-27 15:28:38 +0300

    tr, ar contacts

--- app/content/contacts/contacts.component.ts
+++ app/content/contacts/contacts.component.ts
@@ -24,16 +24,48 @@ export class ContactsComponent {
   For correspondence: 354000; Russian Federation, Krasnodar, Sochi, UL. Vorovskogo, 1, BT, and / I 964<br>
    Director of the National Library - Boris Shalikovich Cholariya<p></p>
   </div>`;
+  html_ab = `<div>
+  <p align="justify"> Postal Code: 384900; The Republic of Abkhazia, Sukhum, ul. Lakoba, d. 111.<br>
+  Tel.: </p><ul><li>(8-10-995)442-664-15
+  </li><li>(8-10-995)442-624-41</li></ul>
+  For correspondence: 354000; Russian Federation, Krasnodar, Sochi, UL. Vorovskogo, 1, BT, and / I 964<br>
+   Director of the National Library - Boris Shalikovich Cholariya<p></p>
+  </div>`;
+
+  html_tr = `<div>
+  <p align="justify"> Postal Code: 384900; The Republic of Abkhazia, Sukhum, ul. Lakoba, d. 111.<br>
+  Tel.: </p><ul><li>٨-١٠-٩٩٥ ١٥-٦٦٤-٤٤٢
+  </li><li>(8-10-995)442-624-41</li></ul>
+   للمراسلة: ٣٥٤٠٠٠ الاتحاد الروسي، كراسندار، سوتشي، شارع فَرفسكفا، مكتب بريد ٩٦٤<br>
+   مدير المكتبة الوطنية - بوريس شاليكوفتش كولاريا<p></p>
+  </div>`;
+
+  html_ar = `<div>
+  <p align="justify">الرمز البريدي: ٣٨٤٩٠٠. جمهورية أبخازيا، سوخومي، لاكوبا ، د. ١١١<br>
+  هاتف: </p><ul><li>٨-١٠-٩٩٥ ١٥-٦٦٤-٤٤٢
+  </li><li>٨-١٠-٩٩٥ ١٥-٦٦٤-٤٤٢</li></ul>
+  للمراسلة: ٣٥٤٠٠٠ الاتحاد الروسي، كراسندار، سوتشي، شارع فَرفسكفا، مكتب بريد ٩٦٤<br>
+   مدير المكتبة الوطنية - بوريس شاليكوفتش كولاريا<p></p>
+  </div>`;
 
   html: string;
 
   constructor(translate: TranslateService) {
-    
+
     if (translate.currentLang === 'ru') {
         this.html = this.html_ru;
     }
     else if (translate.currentLang === 'en') {
         this.html = this.html_en;
+    }
+    else if (translate.currentLang === 'ab') {
+        this.html = this.html_ab;
+    }
+    else if (translate.currentLang === 'tr') {
+        this.html = this.html_tr;
+    }
+    else if (translate.currentLang === 'ar') {
+        this.html = this.html_ar;
     }
 
     translate.onLangChange.subscribe((event: LangChangeEvent) => {
@@ -42,6 +74,15 @@ export class ContactsComponent {
       }
       else if (translate.currentLang === 'en') {
           this.html = this.html_en;
+      }
+      else if (translate.currentLang === 'ab') {
+          this.html = this.html_ab;
+      }
+      else if (translate.currentLang === 'tr') {
+          this.html = this.html_tr;
+      }
+      else if (translate.currentLang === 'ar') {
+          this.html = this.html_ar;
       }
 
     });
