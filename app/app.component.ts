e4964e20bef0738353dcad72045f31aa886c3cd1
Daniel Abzakh <daniel.abzakh@gmail.com>
2016-07-31 13:05:57 +0400

    1- Disable languages except Russian. 2- Disables nav, links that function not-ready.

--- app/app.component.ts
+++ app/app.component.ts
@@ -28,6 +28,8 @@ export class AppComponent implements OnInit{
           userLang = /(ru|en|tr|ar)/gi.test(userLang) ? userLang : 'en';
            // this language will be used as a fallback when a translation isn't found in the current language
           translate.setDefaultLang('en');
+          // Use Russian for now for release 0.1.0
+          userLang = 'ru';
           translate.currentLang = userLang;
            // the lang to use, if the lang isn't available, it will use the current loader to get them
           translate.use(userLang);
