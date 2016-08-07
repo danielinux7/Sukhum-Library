519e52c731ece3cdc62b60c9daa47460d457ddc2
Daniel Abzakh <daniel.abzakh@gmail.com>
2016-08-07 19:05:41 +0400

    Activate calendar section.

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,12 +2,14 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { HomeComponent } from './content/home/home.component';
 import { NewsComponent } from './content/news/news.component';
 import { ContactsComponent } from './content/contacts/contacts.component';
+import { CalendarComponent } from './content/calendar/calendar.component';
 
 const routes: RouterConfig = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'news', component: NewsComponent },
-  { path: 'contacts', component: ContactsComponent }
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'calendar', component: CalendarComponent }
 ];
 
 export const appRouterProviders = [
---
+++ app/content/calendar/calendar.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'lib-calendar',
+  templateUrl: './app/content/calendar/calendar.component.html',
+  styleUrls:  ['./app/content/calendar/calendar.component.css']
+})
+export class CalendarComponent { }
