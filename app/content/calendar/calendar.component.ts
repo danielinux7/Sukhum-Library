3225c9edf1d6bdb547cb1eb516452a4f4faa2d16
Daniel Abzakh <daniel.abzakh@gmail.com>
2016-08-07 21:06:59 +0400

    Support multi-lang for calendar except ab for now.

--- app/content/calendar/calendar.component.ts
+++ app/content/calendar/calendar.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService, LangChangeEvent } from 'ng2-translate/ng2-translate';
 declare var $:any;
+
 @Component({
   selector: 'lib-calendar',
   templateUrl: './app/content/calendar/calendar.component.html',
   styleUrls:  ['./app/content/calendar/calendar.component.css']
 })
 export class CalendarComponent implements OnInit{
+  lang: string;
 
   ngOnInit (){
     $('#calendar').fullCalendar({
         // put your options and callbacks here
+        lang: this.lang
     })
   }
-  
+
+  constructor(translate: TranslateService) {
+    this.lang =  translate.currentLang;
+
+    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      $('#calendar').fullCalendar( 'destroy' );
+      $('#calendar').fullCalendar({
+          // put your options and callbacks here
+          lang: translate.currentLang
+      })
+
+    });
+  }
+
 }
