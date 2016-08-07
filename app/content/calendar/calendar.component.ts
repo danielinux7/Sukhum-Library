f55f5da6c5f3ab72cdd78d16671efb92e00cc990
Daniel Abzakh <daniel.abzakh@gmail.com>
2016-08-07 20:26:47 +0400

    Added calendar plugin.

--- app/content/calendar/calendar.component.ts
+++ app/content/calendar/calendar.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
 @Component({
   selector: 'lib-calendar',
   templateUrl: './app/content/calendar/calendar.component.html',
   styleUrls:  ['./app/content/calendar/calendar.component.css']
 })
-export class CalendarComponent { }
+export class CalendarComponent implements OnInit{
+
+  ngOnInit (){
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+  }
+  
+}
